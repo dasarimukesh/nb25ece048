491fcbcf012e732866eff6c97ed69906fcbab50e
dasarimukesh <vinnivinni298@gmail.com>
2026-09-11 11:35:26 +0530

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swapRef(int &a, int &b) { int t=a; a=b; b=t;}
+void swapPtr(int *a, int *b) { int t=*a; *a=*b; *b=t; } 
+int main() {
+    int x=10, y=20;
+    swapRef(x,y);
+
+cout<<"After swapRef: x=" << x << "y= " <<y<<endl;
+
+    swapPtr(&x,&y);
+    cout<<"After swapPtr: x=" <<x<< "y=" <<y<< endl;
+    int &alis = x;
+    alis=99;
+    cout<<"x via alis= "<<x<<endl;
+    return 0;
+}
+
+
